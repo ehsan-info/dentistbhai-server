1439ad92fccd8c30609775333c05da7412a13f7a
ehsan-info <ehsanurrahman1992@gmail.com>
2022-11-10 15:16:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('dentist Bhai server');
+});
+
+//User: dentistBhaiDbUser
+//pass: RdfrQezptzKe47aW
+
+const uri = "mongodb+srv://dentistBhaiDbUser:RdfrQezptzKe47aW@cluster0.qohz0wa.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('dentistBhai').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            // console.log(service);
+            res.send(service);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.log(error))
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`dentist bhai server running on port : ${port}`)
+});
